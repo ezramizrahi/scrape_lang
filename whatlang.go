dac4b609896d9213c6055feae3109d46e7a1b111
Ezra Mizrahi <32469862+ezramizrahi@users.noreply.github.com>
2020-10-22 09:52:57 +0800

    whatlang.go

---
+++ whatlang.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+  "github.com/abadojack/whatlanggo"
+)
+
+func main() {
+	someText, err := GetSomeText("https://www3.nhk.or.jp/news/")
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println("Text:")
+	fmt.Printf(someText)
+  info := whatlanggo.Detect(someText)
+  fmt.Println("Language:", info.Lang.String(), "ISO: ", info.Lang.Iso6391(), " Confidence: ", info.Confidence)
+}
+
+// GetSomeText gets text using a selector and returns it.
+func GetSomeText(url string) (string, error) {
+
+	// Get the HTML
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+
+	// Convert HTML into goquery document
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	// Gets first text from given element/selector
+  text := doc.Find("a").First().Text()
+	return text, nil
+}
